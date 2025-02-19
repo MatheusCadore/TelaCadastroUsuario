50fe4ab3f6e553ba6d31e3a53d9a568f498bad39
matheus <matheusecadore@gmail.com>
2025-02-19 13:08:18 -0300

    Criação teste da tela inicio

---
+++ src/InicioTela.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class InicioTela extends JFrame {
+    private JPanel contentPane;
+    private JTextField userNome;
+    private JTextField userSenha;
+    private JButton acessarUsuario;
+    private JButton sairDaTela;
+}
