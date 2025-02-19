4cfc4b986973b2808ecdf31d08c989b11cd35f3b
matheus <matheusecadore@gmail.com>
2025-02-18 22:53:57 -0300

    Criação da Classe Usuario

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,2 +1,79 @@
 public class Usuario {
+
+    private String nome;
+    private String sobrenome;
+    private int idade;
+    private int cpf;
+    private int telefone;
+    private String email;
+    private String senha;
+
+
+    public Usuario(
+            String nome, String sobrenome, int idade, int cpf,
+            int telefone, String email, String senha) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.idade = idade;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
